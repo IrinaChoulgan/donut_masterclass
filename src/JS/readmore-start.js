a7ceee56c154c56bff495bb5d9b0331c3403b214
nikonenkoincodeit <test@test.com>
2021-05-17 23:07:09 +0300

    added script js

--- src/JS/readmore-start.js
+++ src/JS/readmore-start.js
@@ -1,5 +1,12 @@
-		$('article').readmore({
-			maxHeight: 240,
-			moreLink: '<a href="#">Подробнее</a>',
-			lessLink: '<a href="#">Скрыть</a>'
-		});
+window.addEventListener('load', function () {
+  const switchableContent = document.querySelector('.switchable-content');
+  const btnToggleContent = document.querySelector('.js-btn-toggle-content');
+
+  function toggleContent() {
+    switchableContent.classList.toggle('is-open');
+
+    this.textContent = switchableContent.classList.contains('is-open') ? 'Less' : 'Read more';
+  }
+
+  btnToggleContent.addEventListener('click', toggleContent);
+});
